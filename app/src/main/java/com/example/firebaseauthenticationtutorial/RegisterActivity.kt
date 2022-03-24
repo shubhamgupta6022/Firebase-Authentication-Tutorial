52c2bb32c62e8bbe35be8c1e234b1c88d67d59df
shubhamgupta <shubhamgupta6022@gmail.com>
2022-03-24 12:30:15 +0530

    Register user functionality added

--- app/src/main/java/com/example/firebaseauthenticationtutorial/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/RegisterActivity.kt
@@ -1,21 +1,68 @@
 package com.example.firebaseauthenticationtutorial
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Patterns
+import androidx.appcompat.app.AppCompatActivity
 import com.example.firebaseauthenticationtutorial.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.buttonRegister.setOnClickListener {
+            val email = binding.textEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+
+            if (email.isEmpty()) {
+                binding.textEmail.error = "Email Required"
+                binding.textEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.textEmail.error = "Valid Email Required"
+                binding.textEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty() || password.length < 6) {
+                binding.editTextPassword.error = "6 char password required"
+                binding.editTextPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            registerUser(email, password)
+        }
+
         binding.textViewLogin.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //Register success
+                    val intent = Intent(this, HomeActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    startActivity(intent)
+                } else {
+                    task.exception?.message?.let {
+                        toast(it)
+                    }
+                }
+            }
     }
 }
