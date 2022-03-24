da4ee74a7fe50224972022814c04d6f49e3aba47
shubhamgupta <shubhamgupta6022@gmail.com>
2022-03-24 11:49:47 +0530

    Adding data binding

--- app/src/main/java/com/example/firebaseauthenticationtutorial/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/LoginActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.firebaseauthenticationtutorial.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val registerButton=findViewById<TextView>(R.id.text_view_register)
-        registerButton.setOnClickListener {
+        binding.textViewRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
--- app/src/main/java/com/example/firebaseauthenticationtutorial/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/RegisterActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.firebaseauthenticationtutorial.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val loginButton=findViewById<TextView>(R.id.text_view_login)
-        loginButton.setOnClickListener {
+        binding.textViewLogin.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
