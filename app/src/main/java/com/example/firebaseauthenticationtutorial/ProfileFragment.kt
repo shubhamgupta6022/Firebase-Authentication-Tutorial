97626d33253f77f1e0d078ee14ac033843c679e3
shubhamgupta <shubhamgupta6022@gmail.com>
2022-03-24 15:56:17 +0530

    Uploading Profile Picture

--- app/src/main/java/com/example/firebaseauthenticationtutorial/ProfileFragment.kt
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/ProfileFragment.kt
@@ -1,19 +1,93 @@
 package com.example.firebaseauthenticationtutorial
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.firebaseauthenticationtutorial.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
+
+    private val TAG = ProfileFragment::class.java.simpleName
+
+    private lateinit var binding: FragmentProfileBinding
+    private val REQUEST_IMAGE_CAPTURE = 100
+    private lateinit var imageUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imageView.setOnClickListener {
+            takePictureIntent()
+        }
+    }
+
+    private fun takePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { pictureIntent ->
+            pictureIntent.resolveActivity(activity?.packageManager!!).also {
+                startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            uploadImageAndSaveUri(imageBitmap)
+        }
+    }
+
+    private fun uploadImageAndSaveUri(bitmap: Bitmap) {
+        val btos = ByteArrayOutputStream()
+        val storageRef =
+            FirebaseStorage.getInstance()
+                .reference
+                .child("pics/${FirebaseAuth.getInstance().currentUser?.uid}")
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, btos)
+        val image = btos.toByteArray()
+        val upload = storageRef.putBytes(image)
+
+        binding.progressbarPic.visibility = View.VISIBLE
+        upload.addOnCompleteListener { uploadTask ->
+            binding.progressbarPic.visibility = View.INVISIBLE
+            if (uploadTask.isSuccessful) {
+                storageRef.downloadUrl.addOnCompleteListener { urlTask ->
+                    urlTask.result?.let {
+                        imageUri = it
+                        activity?.toast(imageUri.toString())
+                        binding.imageView.setImageBitmap(bitmap)
+                    }
+                }
+            } else {
+                uploadTask.exception?.let {
+                    activity?.toast(it.message!!)
+                    Log.v(TAG, it.message!!)
+                }
+            }
+        }
+
+    }
 }
