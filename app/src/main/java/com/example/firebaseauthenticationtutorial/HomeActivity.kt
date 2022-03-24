b2e7dabb162730d5f29ce18f21710aed41612363
shubhamgupta <shubhamgupta6022@gmail.com>
2022-03-24 15:04:10 +0530

    Navigation drawer added

--- app/src/main/java/com/example/firebaseauthenticationtutorial/HomeActivity.kt
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/HomeActivity.kt
@@ -2,10 +2,36 @@ package com.example.firebaseauthenticationtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.firebaseauthenticationtutorial.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment),
+            binding.drawerLayout
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
     }
 }
